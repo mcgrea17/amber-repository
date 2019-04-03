657f9a81fffd94d445a14e8de4f812bb40801d45
Amber McGregor <amber@vqhomes.com>
2019-04-03 14:06:26 +0000

    Added quotes

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_04_03_133354) do
 
   create_table "quotes", force: :cascade do |t|
     t.string "saying"
-    t.string "authoer"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
