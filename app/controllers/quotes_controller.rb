b0dbaf19f67b8021fe736d48b9f4404d8e342435
Amber McGregor <amber@vqhomes.com>
2019-04-05 03:57:31 +0000

    Added validations

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,7 +8,13 @@ class QuotesController < ApplicationController
   end
 
   def create
-    Quote.create(quote_params)
+    
+    @quote = Quote.create(quote_params)
+
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+
     redirect_to quotes_path
   end
 
